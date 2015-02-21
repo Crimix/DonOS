2d59502608cfd6bea77859669a7885f20f94b6d6
Christoffer Mouritzen <christofferm@live.dk>
2015-02-21 18:51:49 +0100

    Added msg to user

--- updateAll.lua
+++ updateAll.lua
@@ -1,4 +1,6 @@
 local list={}
+local i = 0
+local j = 0
 baseUrl = "https://raw.githubusercontent.com/Crimix/DonOS/master/"
 
 function set(name,url)
@@ -10,6 +12,12 @@ function makeList()
 	set("startup","startup.lua")
 end
 
+function getCount()
+	for name, data in pairs(list) do
+		i=i+1
+	end
+end
+
 function update()
 	for name, data in pairs(list) do
 		file = http.get(baseUrl..data["url"])
@@ -17,10 +25,12 @@ function update()
 			f = fs.open(name,"w")
 			f.write(file.readAll())
 			f.close()
+			j=j+1
 		end
 	end
 end
 
 makeList()
 update()
+print("Updating done "..j.." Out of "..i)
 		
