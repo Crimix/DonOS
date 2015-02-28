2274ceb69087996f9e10c213736996d1f6209e82
Christoffer Mouritzen <christofferm@live.dk>
2015-02-28 14:45:14 +0100

    Fixed a derp

--- DonOS/Computer/Reactor/startup.lua
+++ DonOS/Computer/Reactor/startup.lua
@@ -15,14 +15,14 @@ function reactor()
 	sleep(2)
 end
 
--- function rednet()
-	-- pocket1 = DonRednetRef.pocket1
-	-- pocket2 = DonRednetRef.pocket2
-	-- local id,msg,pro = rednet.receive(2)
-	-- if ((id = pocket1) or (id = pocket2)) then
-		-- shell.run("DonOS/Computer/Reactor/"..msg)
-	-- end
--- end
+function rednet()
+	pocket1 = DonRednetRef.pocket1
+	pocket2 = DonRednetRef.pocket2
+	local id,msg,pro = rednet.receive(2)
+	if ((id = pocket1) or (id = pocket2)) then
+		shell.run("DonOS/Computer/Reactor/"..msg)
+	end
+end
 
---parallel.waitForAny(reactor,rednet)
+parallel.waitForAny(reactor,rednet)
 
