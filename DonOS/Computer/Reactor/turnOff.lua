1aabf486974b4c1c0e7476fefcfc438379860253
Christoffer Mouritzen <christofferm@live.dk>
2015-06-28 18:54:57 +0200

    Should fix the reactor bug

--- DonOS/Computer/Reactor/turnOff.lua
+++ DonOS/Computer/Reactor/turnOff.lua
@@ -2,6 +2,6 @@ print("Reactor Off")
 
 br = peripheral.wrap(DonWrap.wrap(DonWrapList.reactor))
 br.setActive(false)
-if(DonWrap.wrap(DonWrapList.capBank)) not nil)then
+if(not(DonWrap.wrap(DonWrapList.capBank) == nil))then
 	rs.setOutput(DonWrap.wrap(DonWrapList.capBank),false)
 end
--- DonOS/Computer/Reactor/turnOn.lua
+++ DonOS/Computer/Reactor/turnOn.lua
@@ -2,6 +2,6 @@ print("Reactor On")
 
 br = peripheral.wrap(DonWrap.wrap(DonWrapList.reactor))
 br.setActive(true)
-if(DonWrap.wrap(DonWrapList.capBank)) not nil)then
+if(not(DonWrap.wrap(DonWrapList.capBank) == nil))then
 	rs.setOutput(DonWrap.wrap(DonWrapList.capBank),true)
 end
